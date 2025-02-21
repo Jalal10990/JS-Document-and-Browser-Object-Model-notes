50e699fd6e6ba868734411f8b23feb6e99baf32b
Shah jalal <jalal.chishti122448@gmail.com>
2025-02-20 19:39:24 -0800

    JS Document and Browser Object Model  notes

---
+++ script.js
@@ -0,0 +1,6 @@
+document.title = " Js is important for Web"
+console.log(document.title)
+console.log(document.body)
+document.body.style.backgroundColor ="green";
+// BOM 
+window.document.body.style.backgroundColor ="yellow";
